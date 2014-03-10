d9c80dae7f4185a6e647e0908c6ec6084a58c61b
Matt Tucker <Theshaft@Matts-Mac-mini.local>
2014-03-09 19:21:19 -0700

    skeleton layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bomb::Application.config.secret_key_base = 'a3baddf30c3ee67b693df2d2f1aac00d89e58c2b80bf9869cb2b8a040648450ea5e84473e40cf46aa148a2829742707f75160c79a7984dfbc88bcd88aa780d22'
